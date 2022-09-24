165303320fce3df1caa94b83df4702beda7906fa
Saif Ali Khan <contact.techosaif@gmail.com>
2022-09-25 01:21:04 +0530

    - Added icons package. - Added top nav bar styles.

--- components/navbars/top-nav-bar.component.tsx
+++ components/navbars/top-nav-bar.component.tsx
@@ -1,4 +1,6 @@
 import { FunctionComponent } from 'react'
+import { FaGoogleWallet, FaArtstation } from 'react-icons/fa'
+import { RiMoneyDollarCircleFill } from 'react-icons/ri'
 
 interface IProps {
 
@@ -7,9 +9,62 @@ interface IProps {
 const TopNavBarComponent: FunctionComponent<IProps> = (props) => {
     return (
         <div className='bg-app-gray2 h-20'>
-            <div className='flex flex-row h-full'>
-                
-           </div>
+            <div className='flex flex-row flex-wrap justify-between items-center h-full w-full px-10 py-5'>
+                <div>
+                    <div className='inline-flex flex-wrap items-center'>
+                        <div className='text-xl  uppercase font-semibold'>
+                            Good Morning!
+                        </div>
+                        <div className='mx-3 text-gray-500'>
+                            |
+                        </div>
+                        <div className='text-xs flex items-center'>
+                            Augest 10, 2022
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='inline-flex flex-wrap items-center'>
+                        <div className='text-gray-600 font-extrabold text-sm'>
+                            Gas Fee
+                        </div>
+                        <div className='text-gray-500 text-xs mx-2'>
+                            Avg
+                        </div>
+                        <div className='text-green-500 text-sm'>
+                            9 GWEI
+                        </div>
+                    </div>
+                    <div className='inline-flex flex-wrap items-center px-10'>
+                        <div className='text-gray-600 font-extrabold text-sm'>
+                            Prices
+                        </div>
+                        <div className='text-gray-500 mx-2'>
+                            <FaArtstation />
+                        </div>
+                        <div className='text-sky-500 text-sm'>
+                            $1600
+                        </div>
+                    </div>
+                    <div className='inline-flex flex-wrap items-center'>
+                        <div className='text-gray-600 font-extrabold text-sm'>
+                            Prices
+                        </div>
+                        <div className='text-gray-500 mx-2'>
+                            <RiMoneyDollarCircleFill />
+                        </div>
+                        <div className='text-violet-500 text-sm'>
+                            $35
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <button className='rounded-full font-semibold border border-white text-sm px-5 py-1 inline-flex items-center'>
+                        <FaGoogleWallet />&nbsp;
+                        Connect Wallet
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
