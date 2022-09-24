6028ae590f0f3b6f8dc2d020e42c61dcfa0c245d
Saif Ali Khan <contact.techosaif@gmail.com>
2022-09-24 22:03:09 +0530

    Configured chakra-ui and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,tsx}",
+    "./components/**/*.{html,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Work Sans']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
